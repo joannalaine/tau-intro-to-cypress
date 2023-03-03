d1b43619923c64c0b8fa6ed89ae246b817b9ade5
Joanna Denni <joannanoriega@gmail.com>
2023-03-03 15:11:44 -0800

    feat: add validations

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -3,6 +3,12 @@
 it("should be able to add, complete, and clear a todo", () => {
   cy.visit("http://todomvc-app-for-testing.surge.sh");
   cy.get(".new-todo").type("Clean kitchen{enter}");
+  cy.get("label").should("have.text", "Clean kitchen");
+
+  cy.get(".toggle").should("not.be.checked");
   cy.get(".toggle").click();
+  cy.get("label").should("have.css", "text-decoration-line", "line-through");
+
   cy.contains("Clear").click();
+  cy.get(".todo-list").should("not.have.descendants", "li");
 });

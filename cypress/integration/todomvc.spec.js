f16902664d64a0efa70090923222f71f8ab85c4f
Joanna Denni <joannanoriega@gmail.com>
2023-03-03 14:07:12 -0800

    feat: lesson 1 - access and interact with elements

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
-it("should navigate to TodoMVC app", () => {
+it("should be able to add, complete, and clear a todo", () => {
   cy.visit("http://todomvc-app-for-testing.surge.sh");
+  cy.get(".new-todo").type("Clean kitchen{enter}");
+  cy.get(".toggle").click();
+  cy.contains("Clear").click();
 });

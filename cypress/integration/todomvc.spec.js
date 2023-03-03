f2058ee6e1ea3d986031fd3d0633da75579e4e87
Joanna Denni <joannanoriega@gmail.com>
2023-03-03 15:29:06 -0800

    refactor: split and regroup tests into describe block

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,14 +1,24 @@
 /// <reference types="cypress" />
 
-it("should be able to add, complete, and clear a todo", () => {
-  cy.visit("http://todomvc-app-for-testing.surge.sh");
-  cy.get(".new-todo").type("Clean kitchen{enter}");
-  cy.get("label").should("have.text", "Clean kitchen");
+describe("todo actions", () => {
+  beforeEach(() => {
+    cy.visit("http://todomvc-app-for-testing.surge.sh");
+    cy.get(".new-todo").type("Clean kitchen{enter}");
+  });
 
-  cy.get(".toggle").should("not.be.checked");
-  cy.get(".toggle").click();
-  cy.get("label").should("have.css", "text-decoration-line", "line-through");
+  it("should add a new todo to the list", () => {
+    cy.get("label").should("have.text", "Clean kitchen");
+    cy.get(".toggle").should("not.be.checked");
+  });
 
-  cy.contains("Clear").click();
-  cy.get(".todo-list").should("not.have.descendants", "li");
+  it("should mark a todo as completed", () => {
+    cy.get(".toggle").click();
+    cy.get("label").should("have.css", "text-decoration-line", "line-through");
+  });
+
+  it("should clear completed todos", () => {
+    cy.get(".toggle").click();
+    cy.contains("Clear").click();
+    cy.get(".todo-list").should("not.have.descendants", "li");
+  });
 });

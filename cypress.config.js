0cfbcfd2107cf83b92bfc06cf6935b1e8c04c5fb
Joanna Denni <joannanoriega@gmail.com>
2023-03-03 23:33:28 -0800

    chore: update cypress to install applitools

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
@@ -6,4 +6,14 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+})
+
+const eyesPlugin = require('@applitools/eyes-cypress')
+module.exports = eyesPlugin(
+  defineConfig({
+    // the e2e or component configuration
+    e2e: {
+      setupNodeEvents(on, config) {},
+    },
+  }),
+)
